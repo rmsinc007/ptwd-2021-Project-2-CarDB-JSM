e6eeecc1abee83ffb761893cd7561d19fa864f51
JSirial429 <Jos0928@gmail.com>
2021-04-08 21:01:01 -0400

    added layout.hbs file in views dir

--- app.js
+++ app.js
@@ -7,5 +7,17 @@ app.set('views engine', 'hbs');
 app.set(path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname,'public')));
 
+
+//Routes
+app.get('/', (req,res) =>{
+
+    console.log('Hello World');
+
+});
+
+app.get('/add', (req, res) =>{
+    console.log('Add car!');
+});
+
 app.listen(3000, ()=>console.log('🏃‍ on port 3000'));
 
