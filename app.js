3bb18bb3e68a8a0a173a365819708d2be49a96bb
Matthew Solomon <matthewsolomon@Matthews-MacBook-Pro.local>
2021-04-10 17:12:23 -0400

    add cookieParser and password req to User model

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const hbs = require('hbs');
 const path = require('path');
+const cookieParser = require('cookie-parser');
 
 //User authentication
 const router = require('express').Router();
--- models/User.model.js
+++ models/User.model.js
@@ -9,11 +9,12 @@ const userSchema = new Schema({
     required: true,
     unique: true,
   },
+  password: String,
   phone: { type: String, required: true },
   city:  { type: String, required: true },
   state: { type: String, required: true }
 });
 
-const User = mongoose.model('User'. userSchema);
+const User = model('User'. userSchema);
 
 module.exports = User;
