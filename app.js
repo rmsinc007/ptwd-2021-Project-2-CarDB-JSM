ae925658fd1ea21560b3305a57076d872872f94f
JSirial429 <Jos0928@gmail.com>
2021-04-10 12:46:04 -0400

    fixed issue with HBS view engine

--- app.js
+++ app.js
@@ -3,19 +3,22 @@ const app = express();
 const hbs = require('hbs');
 const path = require('path');
 
-app.set('views engine', 'hbs');
-app.set(path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname,'public')));
 
 
 //Routes
 app.get('/', (req,res) =>{
 
+    res.render('login');
     console.log('Hello World');
 
 });
 
 app.get('/add', (req, res) =>{
+
+    res.render("signup")
     console.log('Add car!');
 });
 
