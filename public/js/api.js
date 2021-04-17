0ba79831a216002c497fdd3ace6b887bcf800fb1
Spencer Soto <spencer.soto@gmail.com>
2021-04-17 13:19:20 -0400

    news api

--- public/js/api.js
+++ public/js/api.js
@@ -1,10 +1,7 @@
-//const { default: axios } = require("axios");
-
 const newsInput     = document.querySelector('#news-article');
 const getNewsButton = document.querySelector('#get-news-button');
 const covidNews     = document.querySelector('#news-article');
 
-import axios from "axios";
 
 const options = {
   method: 'GET',
@@ -22,12 +19,19 @@ axios.request(options).then(function (response) {
 });
 
 getNewsButton.addEventListener( "click", () => {
-  console.log("click");
   const newsArticle = newsInput.value;
-  axios
-    .get(`https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/news/get-coronavirus-news/${newsArticle}`)
-    .then((responseFromAPI) => {
-        console.log(responseFromAPI.data[0]);
-        const {name, article } = responseFromAPI.data[0];
-    });
+  console.log("click");
+  const options = {
+    method: 'GET',
+    url: `https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/news/get-coronavirus-news/0`,
+    headers: {
+      'x-rapidapi-key': 'a678a8a890msh7a3034aee5fcb2fp167021jsn4cccf885c45e',
+      'x-rapidapi-host': 'vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com'
+    }
+  };
+  axios.request(options).then(function (response) {
+    console.log(response.data);
+  }).catch(function (error) {
+    console.error(error);
+  });
 });
--- routes/covid-news.routes.js
+++ routes/covid-news.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+
 //route for news feed
 router.get('/news', (req,res,next)=>{
     res.render('covid-news');
