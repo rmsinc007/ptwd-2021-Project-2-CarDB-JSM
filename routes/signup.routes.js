26591ef67b591962faea134a9e8f1cdae7036298
Spencer Soto <spencer.soto@gmail.com>
2021-04-18 14:02:15 -0400

    news api

--- routes/signup.routes.js
+++ routes/signup.routes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-
+//route for support page
+router.get('/signup', (req,res,next)=>{
+    res.render('signup');
+});
 
 
 module.exports = router;
