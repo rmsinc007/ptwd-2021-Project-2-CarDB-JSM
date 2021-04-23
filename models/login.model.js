1e1965a4ec137a8b0ade3e1addc545b1f803faa9
Matthew Solomon <matthewsolomon@Matthews-MacBook-Pro.local>
2021-04-22 20:19:07 -0400

    update login

--- models/login.model.js
+++ models/login.model.js
@@ -1,7 +1,6 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const { Schema, model } = require("mongoose");
 
-const loginModel = Schema ({
+const loginModel =new Schema ({
     email:{
         type: String,
         required: true,
--- routes/signup.routes.js
+++ routes/signup.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router  = express.Router();
-//const Login   = require(".models/login.model.js");
+const Login   = require("../models/login.model.js");
 const bcrypt  = require("bcryptjs");
 
 //route for support page
