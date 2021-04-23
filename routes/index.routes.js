f9ce381b1ed7757850688ae4db8c353725357398
Spencer Soto <spencer.soto@gmail.com>
2021-04-22 20:38:05 -0400

    login code

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -7,6 +7,55 @@ router.get("/", (req, res, next) => {
     res.render("login");
 });
 
+router.post("/login", async (req, res, next) => {
+    console.log("Session", req.session);
+    const { email, password } = req.body;
+    if (email === "" || password === "") {
+      res.render("profile", { errorMessage: "All the fields are mandatory" });
+    } else {
+      // try {
+      //   const userFromDB = await User.find({ email });
+      //   console.log(userFromDB);
+      // } catch (err) {
+      //   next(err);
+      // }
+      // findOne will return the first match against your query from the DB
+      User.find({ email })
+        .then((userArrFromDB) => {
+          //  no user with such email -> show error
+          // email matches something => [{ email: dasdas, pass: dsda }]
+          // doesn't match => []
+          if (userArrFromDB.length === 0) {
+            res.render("auth/login", {
+              errorMessage: "User does not exist in the DB",
+            });
+          } else {
+            // check if password is correct
+            const passwordMatch = bcrypt.compareSync(
+              password,
+              userArrFromDB[0].password
+            );
+            if (!passwordMatch) {
+              res.render("auth/login", {
+                errorMessage: "Auth does not match",
+              });
+            } else {
+              userArrFromDB[0].password = "";
+              req.session.user = userArrFromDB[0];
+              // find user redirect to profile
+              res.render("profile");
+            }
+          }
+        })
+        .catch((err) => next(err));
+    }
+  });
+  
+  router.get("/logout", (req, res, next) => {
+    req.session.destroy();
+    res.redirect("/");
+  });
+
 
 
 module.exports = router;
